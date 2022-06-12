25220edc6acb0b7adf0a06048f4d105963dd1472
Didrik Nordström <didrik.nordstrom@gmail.com>
2022-06-12 12:21:32 -0700

    fix: macos support

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
-use std::path::PathBuf;
+#[cfg(target_os = "macos")]
+mod macos;
+
+#[macro_use]
+#[cfg(target_os = "macos")]
+extern crate objc;
 
 #[cfg(target_os = "linux")]
 use gtk::{gdk::SELECTION_CLIPBOARD, Clipboard};
 
 #[cfg(target_os = "windows")]
-use clipboard_win::{Clipboard, formats, Getter, get_clipboard};
+use clipboard_win::{formats, get_clipboard, Clipboard, Getter};
 
 #[derive(Debug)]
 pub enum Error {
     NoFiles,
+    SystemError(String),
 }
 
 fn main() {
     println!("Hello, world!");
-    let files = get_files_windows().unwrap();
+    let files = get_files_macos().unwrap();
     println!("files: {:?}", files);
+}
+
+#[cfg(target_os = "macos")]
+fn get_files_macos() -> Result<Vec<String>, Error> {
+    let clipboard = macos::Clipboard::new().unwrap();
+    Ok(clipboard.read().unwrap())
 }
 
 #[cfg(target_os = "linux")]
@@ -34,7 +46,7 @@ fn get_files_linux() -> Result<Vec<String>, Error> {
 
 #[cfg(target_os = "windows")]
 fn get_files_windows() -> Result<Vec<String>, Error> {
-    get_clipboard(formats::FileList{}).map_err(|_| Error::NoFiles)
+    get_clipboard(formats::FileList {}).map_err(|_| Error::NoFiles)
     // let _clip = Clipboard::new_attempts(10).expect("Open clipboard");
     // let filelist = formats::FileList{};
     // let mut out: Vec<String> = Vec::new();
@@ -42,4 +54,3 @@ fn get_files_windows() -> Result<Vec<String>, Error> {
     // let n = filelist.read_clipboard(&mut out).unwrap();
     // Ok(out)
 }
-
