0adb071013346a5ecda18f438f8ddbbc3c273b84
Didrik Nordström <didrik.nordstrom@gmail.com>
2022-06-12 15:00:43 -0700

    fix: simplify example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,8 +1,5 @@
 use clipboard_files::read;
 
 fn main() {
-    let files = read();
-    println!("files (1): {:?}", files);
-    let files = read();
-    println!("files (2): {:?}", files);
+    println!("files: {:?}", read());
 }
