091dd0fe4c08a1b11ec8ac17df73c20e0a2a8a06
Didrik Nordstrom <didrik.nordstrom@gmail.com>
2022-06-11 19:30:31 -0700

    fix: windows support

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use std::path::PathBuf;
 #[cfg(target_os = "linux")]
 use gtk::{gdk::SELECTION_CLIPBOARD, Clipboard};
 
+#[cfg(target_os = "windows")]
+use clipboard_win::{Clipboard, formats, Getter, get_clipboard};
+
 #[derive(Debug)]
 pub enum Error {
     NoFiles,
@@ -10,7 +13,7 @@ pub enum Error {
 
 fn main() {
     println!("Hello, world!");
-    let files = get_files_linux().unwrap();
+    let files = get_files_windows().unwrap();
     println!("files: {:?}", files);
 }
 
@@ -28,3 +31,15 @@ fn get_files_linux() -> Result<Vec<String>, Error> {
         .map(|path| urlencoding::decode(&path.as_str()).unwrap().into_owned())
         .collect())
 }
+
+#[cfg(target_os = "windows")]
+fn get_files_windows() -> Result<Vec<String>, Error> {
+    get_clipboard(formats::FileList{}).map_err(|_| Error::NoFiles)
+    // let _clip = Clipboard::new_attempts(10).expect("Open clipboard");
+    // let filelist = formats::FileList{};
+    // let mut out: Vec<String> = Vec::new();
+    // // this unwrap fails if not files
+    // let n = filelist.read_clipboard(&mut out).unwrap();
+    // Ok(out)
+}
+
